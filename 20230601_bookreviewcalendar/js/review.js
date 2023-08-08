07f8113fef70da601d91cac00fc28d08c15f0131
LO-VE-0 <hsg151515@naver.com>
2023-08-08 16:10:00 +0900

    URL parameter book -> String->Number -> id 비교 -> HTML->js -> innerHTMl

---
+++ 20230601_bookreviewcalendar/js/review.js
@@ -0,0 +1,45 @@
+// url에 있는 book parameter 값(String) -> books의 인덱스로 변환 -> 
+const bookId = new URL(window.location.href).searchParams.get('book');
+console.log(`bookID: ${bookId}, type: ${typeof bookId}`);
+
+
+// String을 int로 변환 -> -1 -> books의 인덱스로 변환 (int)
+let bookIdNumber = Number(bookId);   // string -> number
+// let bookIdNumber = parseInt(bookId);   // string -> number
+// let bookIdNumber = bookId * 1;   // string -> number
+// let bookIdNumber = bookId + 0;   // 20 (string -> number XXXX)
+
+console.log(`bookIdNumber: ${bookIdNumber}, type: ${typeof bookIdNumber}`);
+
+
+// -> books에서 하나씩 꺼내어 bookIdNumber랑 꺼낸 book의 id랑 같으면 데이터 빼자: bookData
+// bookIdNumber: Url에서 가져온 id값 === books 하나씩 꺼낸 book의 id 값
+let bookData;
+for (let book of books) {
+    if (bookIdNumber === book.id) {
+        bookData = book;    // id가 같으면, 그를 꺼내서 bookData에 넣자
+        break;
+    }
+}
+// console.log(bookData);
+
+
+// -> title, author, publisher, bookImage 알아내자
+const title = bookData.title;
+// const title = bookData["title"];
+const author = bookData.author;
+const publisher = bookData.publisher;
+const bookImage = bookData.img;
+console.log(title, author, publisher, bookImage);
+
+// -> HTML에 넣자
+// HTML 요소 -> JS 변수
+const titleDiv = document.getElementsByClassName("title")[1];
+const authorDiv = document.getElementsByClassName("author")[1];
+const publisherDiv = document.getElementsByClassName("publisher")[1];
+const bookImageDiv = document.getElementsByClassName("book-image")[0];
+
+titleDiv.innerHTML = title;
+authorDiv.innerHTML = author;
+publisherDiv.innerHTML = publisher;
+bookImageDiv.innerHTML = `<img src='${bookImage}'>`;
