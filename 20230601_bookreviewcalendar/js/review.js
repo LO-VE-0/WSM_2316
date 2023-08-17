dd2ea60221dbba108215b9cd0450c4784811d321
LO-VE-0 <hsg151515@naver.com>
2023-08-17 10:42:26 +0900

    읽은 날짜에 오늘 날짜 넣자

--- 20230601_bookreviewcalendar/js/review.js
+++ 20230601_bookreviewcalendar/js/review.js
@@ -46,8 +46,22 @@ const titleDiv = document.getElementsByClassName("title")[1];
 const authorDiv = document.getElementsByClassName("author")[1];
 const publisherDiv = document.getElementsByClassName("publisher")[1];
 const bookImageDiv = document.getElementsByClassName("book-image")[0];
+const readDateDiv = document.getElementsByClassName("read-date")[1];
+
+
+let now = "2030년 8월 17일";
+now = new Date(); // 현재 날짜와 시간 객체
+let year = now.getFullYear();
+let month = now.getMonth()+1;
+let date = now.getDate();
+const WEEKDAY = ['일', '월', '화', '수', '목', '금', '토'];
+let day = WEEKDAY[now.getDay()];
+now = `${year}년 ${month}월 ${date}일 ${day}요일`
+
+// now = `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일`
 
 titleDiv.innerHTML = title;
 authorDiv.innerHTML = author;
 publisherDiv.innerHTML = publisher;
 bookImageDiv.innerHTML = `<img src='${bookImage}'>`;
+readDateDiv.innerHTML= now;
